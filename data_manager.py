056e9a5529672666a8b32acf84b373442b60ea25
matCsikai <ccsmatyas@gmail.com>
2017-07-12 10:37:46 +0200

    datamanager modified from csv to sql, requirements.txt, setenv.sh.template created

--- data_manager.py
+++ data_manager.py
@@ -1,32 +1,104 @@
-import csv
+import os
+import psycopg2
 
 
-def import_csv_file(file_name="food_database.csv"):
-    database_as_list = []
-    with open(file_name, newline="") as csv_file:
-        imported_database = csv.reader(csv_file, delimiter=',', quotechar="|")
-        for row in imported_database:
-            database_as_list.append(str(row))
-    return database_as_list
+def establish_connection(connection_data=None):
+    """
+    Create a database connection based on the :connection_data: parameter
+
+    :connection_data: Connection string attributes
+
+    :returns: psycopg2.connection
+    """
+    if connection_data is None:
+        connection_data = get_connection_data()
+    try:
+        connect_str = "dbname={} user={} host={} password={}".format(connection_data['dbname'],
+                                                                     connection_data['user'],
+                                                                     connection_data['host'],
+                                                                     connection_data['password'])
+        conn = psycopg2.connect(connect_str)
+        conn.autocommit = True
+    except psycopg2.DatabaseError as e:
+        print("Cannot connect to database.")
+        print(e)
+    else:
+        return conn
 
 
-# read file into a @table
-#
-# @file_name: string
-# @table: list of lists of strings
-def get_table_from_file(file_name):
-    with open(file_name, "r") as file:
-        lines = file.readlines()
-    table = [element.replace("\n", "").split(";") for element in lines]
-    return table
+def get_connection_data(db_name=None):
+    """
+    Give back a properly formatted dictionary based on the environment variables values which are started
+    with :MY__PSQL_: prefix
+
+    :db_name: optional parameter. By default it uses the environment variable value.
+    """
+    if db_name is None:
+        db_name = os.environ.get('MY_PSQL_DBNAME')
+
+    return {
+        'dbname': db_name,
+        'user': os.environ.get('MY_PSQL_USER'),
+        'host': os.environ.get('MY_PSQL_HOST'),
+        'password': os.environ.get('MY_PSQL_PASSWORD')
+    }
 
 
-# write a @table into a file
-#
-# @file_name: string
-# @table: list of lists of strings
-def write_table_to_file(file_name, table):
-    with open(file_name, "w") as file:
-        for record in table:
-            row = ';'.join(record)
-            file.write(row + "\n")
+def execute_script_file(file_path):
+    """
+    Execute script file based on the given file path.
+    Print the result of the execution to console.
+
+    Example:
+    > execute_script_file('db_schema/01_create_schema.sql')
+
+    :file_path: Relative path of the file to be executed.
+    """
+    package_directory = os.path.dirname(os.path.abspath(__file__))
+    full_path = os.path.join(package_directory, file_path)
+    with open(full_path) as script_file:
+        with establish_connection() as conn, \
+                conn.cursor() as cursor:
+            try:
+                sql_to_run = script_file.read()
+                cursor.execute(sql_to_run)
+                print("{} script executed successsfully.".format(file_path))
+            except Exception as ex:
+                print("Execution of {} failed".format(file_path))
+                print(ex.args)
+
+
+def execute_select(statement, variables=None):
+    """
+    Execute SELECT statement optionally parameterized
+
+    Example:
+    > execute_select('SELECT %(title)s;', variables={'title': 'Codecool'})
+
+    :statment: SELECT statement
+
+    :variables:  optional parameter dict"""
+    result_set = []
+    with establish_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(statement, variables)
+            result_set = cursor.fetchall()
+    return result_set
+
+
+def execute_dml_statement(statement, variables=None):
+    """
+    Execute data manipulation query statement (optionally parameterized)
+
+    :statment: SQL statement
+
+    :variables:  optional parameter dict"""
+    result = None
+    with establish_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(statement, variables)
+            try:
+                result = cursor.fetchone()
+            except psycopg2.ProgrammingError as pe:
+                pass
+    return result
